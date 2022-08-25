3497863940cfd9bb6a202e051c514098074b6c65
Baz Murphy <bazmurphy@gmail.com>
2022-08-25 09:21:08 +0100

    rolled back to verbose functional way

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get("/", function (req, res) {
 // app.get("/favicon.ico", function (req, res) {
 //     request.params console.log :
 //     keeps saying "ReferenceError: favicon is not defined"
-//     why do i need to do this???
+//     why do i need to do this? because the page is making a request for a favicon?
 // })
 
 const workOutCity = (city) => {
@@ -52,7 +52,7 @@ const workOutCategory = (city, category) => {
 
 app.get("/:city", function (req, res) {
     // console.log("GET /:city route");
-    console.log(req.params.city);
+    // console.log(req.params.city);
     res.send(workOutCity(req.params.city));
 })
 
@@ -82,27 +82,27 @@ app.get("/:city", function (req, res) {
 
 app.get("/:city/:category", function (req, res) {
 
-    console.log(req.params);
-    // { city: 'stratford', category: 'hospitals' }
-    console.log(req.params.city);
+    // console.log(req.params);
+    // { city: 'stratford', category: 'pharmacies' }
+    // console.log(req.params.city);
     // stratford
-    console.log(req.params.category);
-    // hospitals
+    // console.log(req.params.category);
+    // pharmacies
 
-    const city = req.params.city;
-    const category = req.params.category;
+    // const city = req.params.city;
+    // const category = req.params.category;
     // The second part works because Bracket Notation allows strings..
     // But the first part doesn't work because its just a bare string
     // res.send(city[category]);
 
-    // using EVAL() (everyone online says this is dangerous to use?)
+    // using EVAL() (everyone online says this is dangerous to use)
     // const city = eval(req.params.city)
     // console.log(city);
     // const category = eval(req.params.category);
     // console.log(category)
-    res.send(city[category]);
+    // res.send(city[category]);
     
-    // res.send(workOutCategory(workOutCity(req.params.city), req.params.category));
+    res.send(workOutCategory(workOutCity(req.params.city), req.params.category));
     // ^ this is works, but is ridiculous, there has to be a better way to do this
 })
 
