59f2e7f1d861fc383dfc244584b70b08de45ec1f
Baz Murphy <bazmurphy@gmail.com>
2022-08-25 21:50:33 +0100

    sam helped me with an idea!

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ const stratford = require("./data/Stratford.json");
 const harrow = require("./data/Harrow.json");
 const heathrow = require("./data/Heathrow.json");
 
+const cities = {stratford, harrow, heathrow}
+// ^thank you sam for giving me this idea
+// create a wrapper object so that the req.params "strings" can be used with bracket notation
+
 app.get("/", function (req, res) {
     // console.log("GET / route");
     res.sendFile(__dirname + '/index.html');
@@ -26,84 +30,49 @@ app.get("/", function (req, res) {
 //     why do i need to do this? because the page is making a request for a favicon?
 // })
 
-const workOutCity = (city) => {
-    if (city === "stratford") {
-        return stratford;
-    } else if (city === "harrow") {
-        return harrow;
-    } else if (city === "heathrow") {
-        return heathrow;
-    }
-};
-// ^i don't want to use these functions, i want something more dynamic and contained within the block
+// const workOutCity = (city) => {
+//     if (city === "stratford") {
+//         return stratford;
+//     } else if (city === "harrow") {
+//         return harrow;
+//     } else if (city === "heathrow") {
+//         return heathrow;
+//     }
+// };
 
-const workOutCategory = (city, category) => {
-    if (category === "pharmacies") {
-        return city.pharmacies;
-    } else if (category === "colleges") {
-        return city.colleges;
-    } else if (category === "hospitals") {
-        return city.hospitals;
-    } else if (category === "doctors") {
-        return city.doctors;
-    }
-}
+// const workOutCategory = (city, category) => {
+//     if (category === "pharmacies") {
+//         return city.pharmacies;
+//     } else if (category === "colleges") {
+//         return city.colleges;
+//     } else if (category === "hospitals") {
+//         return city.hospitals;
+//     } else if (category === "doctors") {
+//         return city.doctors;
+//     }
+// }
 // ^i don't want to use these functions, i want something more dynamic and contained within the block
 
 app.get("/:city", function (req, res) {
     // console.log("GET /:city route");
     // console.log(req.params.city);
-    res.send(workOutCity(req.params.city));
+
+    // res.send(workOutCity(req.params.city));
+    // ^ this is works, but is ridiculous, there has to be a better way to do this
+
+    res.send(cities[req.params.city]);
 })
 
-// app.get("/:city/pharmacies", function (req, res) {
-//     // console.log("GET /:city/pharmacies route");
-//     // console.log(req.params.city);
-//     res.send(workOutCity(req.params.city).pharmacies);
-// })
-
-// app.get("/:city/colleges", function (req, res) {
-//     // console.log("GET /:city/colleges route");
-//     // console.log(req.params.city);
-//     res.send(workOutCity(req.params.city).colleges);
-// })
-
-// app.get("/:city/hospitals", function (req, res) {
-//     // console.log("GET /:city/hospitals route");
-//     // console.log(req.params.city);
-//     res.send(workOutCity(req.params.city).hospitals);
-// })
-
-// app.get("/:city/doctors", function (req, res) {
-//     // console.log("GET /:city/doctors route");
-//     // console.log(req.params.city);
-//     res.send(workOutCity(req.params.city).doctors);
-// })
-
 app.get("/:city/:category", function (req, res) {
-
+    // console.log("GET /:city/:category route");
     // console.log(req.params);
     // { city: 'stratford', category: 'pharmacies' }
-    // console.log(req.params.city);
-    // stratford
-    // console.log(req.params.category);
-    // pharmacies
-
-    // const city = req.params.city;
-    // const category = req.params.category;
-    // The second part works because Bracket Notation allows strings..
-    // But the first part doesn't work because its just a bare string
-    // res.send(city[category]);
-
-    // using EVAL() (everyone online says this is dangerous to use)
-    // const city = eval(req.params.city)
-    // console.log(city);
-    // const category = eval(req.params.category);
-    // console.log(category)
-    // res.send(city[category]);
     
-    res.send(workOutCategory(workOutCity(req.params.city), req.params.category));
+    // res.send(workOutCategory(workOutCity(req.params.city), req.params.category));
     // ^ this is works, but is ridiculous, there has to be a better way to do this
+
+    res.send(cities[req.params.city][req.params.category]);
+    // ^thank you sam for giving me this idea, now i can use the string inside bracket notation for "req.params.city"
 })
 
 const listener = app.listen(process.env.PORT || PORT, function() {
